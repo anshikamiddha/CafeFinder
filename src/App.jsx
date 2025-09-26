18dd98941aed8394c29aa041c1ab8244eb40285c
anshikamiddha <150921364+anshikamiddha@users.noreply.github.com>
2025-09-26 22:20:51 +0530

    initial commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,9 @@
-import React from 'react'
+import React from "react";
 
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-import Dashboard from './pages/Dashboard';
-import WriteArticle from './pages/WriteArticle';
-import BlogTitles from './pages/BlogTitles';
-import GenerateImage from './pages/GenerateImage';
-import Removebackground from './pages/Removebackground';
-import RemoveObject from './pages/RemoveObject';
-import ReviewResume from './pages/ReviewResume';
-import Community from './pages/Community';
-import { useAuth } from '@clerk/clerk-react';
-import { useEffect } from 'react';
-import {Toaster} from "react-hot-toast";
-
-const App = () => {
-  const {getToken}=useAuth()
-  useEffect(()=>{
-    getToken().then((token)=>console.log(token));
-  },[]) 
-  
+function App() {
   return (
-  
-    <div>
-      <Toaster/>
-<Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route>
-    <Route path='/ai'  element={<Layout/>}>
-    <Route index element={<Dashboard/>}/>
-    <Route path='WriteArticle' element={<WriteArticle/>}/>
-        <Route path='BlogTitles' element={<BlogTitles/>}/>
-    
-    <Route path='GenerateImage' element={<GenerateImage/>}/>
-    <Route path='Removebackground' element={<Removebackground/>}/>
-    <Route path='RemoveObject' element={<RemoveObject/>}/>
-    <Route path='ReviewResume' element={<ReviewResume/>}/>
-        <Route path='Community' element={<Community/>}/>
-   
-    </Route>
-  </Route>
-</Routes>
- </div>
-   
-  )
+    <h1 className="text-3xl font-bold text-blue-500">hello</h1>
+  );
 }
 
-export default App
+export default App;
